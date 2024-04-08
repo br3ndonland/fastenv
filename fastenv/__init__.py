658fb4b3661374f988a9b101a8655624ec61fb6c
Brendon Smith <bws@bws.bio>
2024-04-08 15:07:59 -0400

    Update to Ruff 0.3
    Ruff 0.3 introduces the 2024.2 code style. Note that the `extend-select`
    configuration setting moves from `tool.ruff` to `tool.ruff.lint`.
    
    https://docs.astral.sh/ruff/settings/
    https://docs.astral.sh/ruff/versioning/
    https://github.com/astral-sh/ruff/blob/main/CHANGELOG.md

--- fastenv/__init__.py
+++ fastenv/__init__.py
@@ -4,6 +4,7 @@ Unified environment variable and settings management for FastAPI and beyond.
 
 https://github.com/br3ndonland/fastenv
 """
+
 try:
     from .cloud.object_storage import ObjectStorageClient, ObjectStorageConfig
 except ImportError:  # pragma: no cover
